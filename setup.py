6d5a148859d934ab14662058a9f8967741368422
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-18 20:22:56 +0530

    added the requiremens and setup

---
+++ setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "GemInsights"
+AUTHOR_USER_NAME = "izam-mohammed"
+SRC_REPO = "gemInsights"
+AUTHOR_EMAIL = "izamdeveloper1@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for ml app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+)
+
+with open("verison.txt", "w") as f:
+    f.write(__version__)
