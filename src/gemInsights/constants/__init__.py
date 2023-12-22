47e1d6faf61b59cca418444d081c815c4fd690ea
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-22 18:33:57 +0530

    changed the prompts to a dedicated folder

--- src/gemInsights/constants/__init__.py
+++ src/gemInsights/constants/__init__.py
@@ -4,7 +4,7 @@ from os import environ
 CONFIG_FILE_PATH = Path("config/config.yaml")
 CREDENTIALS_FILE_PATH = "credentials/cloud_credentials.json"
 PARAMS_FILE_PATH = Path("params.yaml")
-MAIN_PROMPT_FILE_PATH = Path("main_prompt.txt")
+MAIN_PROMPT_FILE_PATH = Path("prompts/main_prompt.txt")
 
 if not Path(CREDENTIALS_FILE_PATH):
     raise KeyError(
